804850f68042f5c7a237fea3ba21985ddcdb6961
zafarirum87 <75549633+zafarirum87@users.noreply.github.com>
2023-05-04 03:54:51 +0300

    calendar added

--- src/components/Calander.js
+++ src/components/Calander.js
@@ -1,17 +1,49 @@
 import React from "react";
+import { useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import timeGridPlugin, { TimeGridView } from "@fullcalendar/timegrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from '@fullcalendar/list';
 
-export default function Calander(){
+export default function Calander() {
 
-    return(
-        <div style={{ margin: '20px', padding: '30px 30px 0px 30px' }}>
+    const [events, setEvents] = React.useState([]);
+    useEffect(() => {
+        // Fetch data from API endpoint
+        fetch('https://traineeapp.azurewebsites.net/gettrainings')
+            .then(response => response.json())
+            .then(data => {
+                // Transform data into array of event objects
+                const newEvents = data.map(training => ({
+                    title: `${training.activity}/${training.customer.firstname} ${training.customer.lastname} (${training.duration} min)`,
+                    start: training.date,
+                    end: (training.date),
+                    // Other properties such as description, color, etc. can be added here
+                    color: 'turkishblue',
+                    borderColor: 'turkishblue',
+                    style: 'border',
+                    display: 'block',
+                }));
+                setEvents(newEvents);
+            })
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div style={{ margin: '20px', padding: '30px 0px 0px 0px' }}>
             <FullCalendar
-                plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
                 initialView={'dayGridMonth'}
-                headerToolbar= {{ left: 'prev today next', center: 'title', right: 'dayGridMonth timeGridWeek timeGridDay'}}
+                views={{
+                    listWeek:  { duration: { days: 7 }, buttonText: 'Agenda'}
+                }}
+                headerToolbar={{
+                    left: 'prev today next',
+                    center: 'title',
+                    right: 'dayGridMonth timeGridWeek timeGridDay listWeek'
+                }}
+                events={events}
             />
         </div>
     )
